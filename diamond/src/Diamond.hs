0526074b6728c55050a4330b3348b93efc13ee22
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-07-01 15:22:44 +0100

    Finished diamond

--- diamond/src/Diamond.hs
+++ diamond/src/Diamond.hs
@@ -1,4 +1,22 @@
 module Diamond (diamond) where
 
+import Data.Char
+
 diamond :: Char -> Maybe [String]
-diamond = error "You need to implement this function"
+diamond c
+  | not $ isAlpha c = Nothing
+  | otherwise = Just (map processRow $ rows $ toUpper c)
+
+processRow :: (Char, Int, Int) -> String
+processRow (c, x, y) = mirror $ replicate x ' ' ++ [c] ++ replicate y ' '
+
+rows :: Char -> [(Char, Int, Int)]
+rows c = mirror (zip3 chars spaceBefore spaceAfter)
+  where
+    chars = ['A' .. c]
+    len = length chars
+    spaceAfter = [0 .. len -1]
+    spaceBefore = reverse spaceAfter
+
+mirror :: [a] -> [a]
+mirror xs = xs ++ tail (reverse xs)
