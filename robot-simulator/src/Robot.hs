bcd9c21fd4ef09d9b550026180268a0b5ce80806
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-07-01 13:50:43 +0100

    Submitted robot-simulator

--- robot-simulator/src/Robot.hs
+++ robot-simulator/src/Robot.hs
@@ -1,27 +1,66 @@
 module Robot
-    ( Bearing(East,North,South,West)
-    , bearing
-    , coordinates
-    , mkRobot
-    , move
-    ) where
+  ( Bearing (East, North, South, West),
+    bearing,
+    coordinates,
+    mkRobot,
+    move,
+  )
+where
 
-data Bearing = North
-             | East
-             | South
-             | West
-             deriving (Eq, Show)
+data Bearing
+  = North
+  | East
+  | South
+  | West
+  deriving (Eq, Show)
 
-data Robot = Dummy
+type Coordinates = (Integer, Integer)
+
+data Robot = Robot Bearing Coordinates
 
 bearing :: Robot -> Bearing
-bearing robot = error "You need to implement this function."
+bearing (Robot b _) = b
 
-coordinates :: Robot -> (Integer, Integer)
-coordinates robot = error "You need to implement this function."
+coordinates :: Robot -> Coordinates
+coordinates (Robot _ c) = c
 
-mkRobot :: Bearing -> (Integer, Integer) -> Robot
-mkRobot direction coordinates = error "You need to implement this function."
+mkRobot :: Bearing -> Coordinates -> Robot
+mkRobot = Robot
 
 move :: Robot -> String -> Robot
-move robot instructions = error "You need to implement this function."
+move robot [] = robot
+move robot (c : cs) = move (instruction robot) cs
+  where
+    instruction :: Robot -> Robot
+    instruction = case c of
+      'A' -> advance
+      'L' -> turnLeft
+      'R' -> turnRight
+      _ -> id
+
+turnLeft :: Robot -> Robot
+turnLeft (Robot b c) = Robot newB c
+  where
+    newB = case b of
+      North -> West
+      East -> North
+      South -> East
+      West -> South
+
+turnRight :: Robot -> Robot
+turnRight (Robot b c) = Robot newB c
+  where
+    newB = case b of
+      North -> East
+      East -> South
+      South -> West
+      West -> North
+
+advance :: Robot -> Robot
+advance (Robot b (x, y)) = Robot b newC
+  where
+    newC = case b of
+      North -> (x, y + 1)
+      East -> (x + 1, y)
+      South -> (x, y - 1)
+      West -> (x - 1, y)
